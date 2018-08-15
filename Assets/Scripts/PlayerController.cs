6f6fe8ef29c2999779412e96de347b64ba1bf44c
GoodComrade <konnov-v@mail.ru>
2018-08-15 15:20:16 +0300

    add the colony quest project files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+    [HideInInspector]
+    public bool facingRight = true;
+    [HideInInspector]
+    public bool jump = false;
+
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbeSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(25f, 25f);
+
+    bool isAlive = true;
+    public float firingRate = 0.2f;
+
+    private Transform groundCheck;
+    private bool grounded = false;
+
+    PlayerController player;
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeet;
+    float gravityScaleAtStart;
+
+    
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        gravityScaleAtStart = myRigidBody.gravityScale;
+        myFeet = GetComponent<BoxCollider2D>();
+        
+    }
+
+    void Update()
+    {
+        if (!isAlive)
+        {
+            return;
+        }
+
+        Run();
+        ClimbLadder();
+        Jump();
+        //FlipSprite();
+        Die();
+
+        float h = Input.GetAxis("Horizontal");
+
+        // If the input is moving the player right and the player is facing left...
+        if (h > 0 && !facingRight)
+            // ... flip the player.
+            Flip();
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (h < 0 && facingRight)
+            // ... flip the player.
+            Flip();
+
+        if (Input.GetMouseButton(0))
+        {
+            myAnimator.SetBool("Attack", true);
+        }
+        else
+        {
+            myAnimator.SetBool("Attack", false);
+        }
+
+        
+            
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //value is between -1 to +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorisontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorisontalSpeed);
+    }
+
+    private void ClimbLadder()
+    {
+        if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myRigidBody.gravityScale = gravityScaleAtStart;
+            myAnimator.SetBool("Climb", false);
+            return;
+        }
+
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        Vector2 climbeVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbeSpeed);
+        myRigidBody.velocity = climbeVelocity;
+        myRigidBody.gravityScale = 0f;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("Climb", playerHasVerticalSpeed);
+
+    }
+
+    private void Jump()
+    {
+        
+
+        if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground", "Foreground1", "Climb", "Physics")))
+        {
+            return;
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+            
+            
+        }
+    }
+
+    private void Die()
+    {
+        if(myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazard")))
+        {
+            isAlive = false;
+            myAnimator.SetTrigger("Die");
+            GetComponent<Rigidbody2D>().velocity = deathKick;
+            FindObjectOfType<GameSessionController>().ProcessPlayerDeath();
+        }
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorisontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorisontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+    }
+
+    void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
